62a9b111014fe79db6d39653e158e5a2a4db6921
Stone Li <119721590+lifengstone@users.noreply.github.com>
2026-02-02 07:00:47 +0000

    update address for testing account

--- course/lesson-1/src/index.ts
+++ course/lesson-1/src/index.ts
@@ -4,9 +4,9 @@ async function main() {
     // faucet https://faucet.polkadot.io/ Paseo AssetHub
     const URL = "https://services.polkadothub-rpc.com/testnet";
     const provider = new ethers.JsonRpcProvider(URL);
-    const address = "0x"; // put your address here
+    const address = "0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac"; // put your address here
     const balance = await provider.getBalance(address);
-    console.log(`Balance of ${address}: ${ethers.formatEther(balance)} ETH`);
+    console.log(`Balance of ${address}: ${ethers.formatEther(balance)} PAS`);
 }
 
 main();
